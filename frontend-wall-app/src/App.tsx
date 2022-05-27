f3689a9d285e7dfdb7496efebc6b790aa2fb9324
vportela <victoria.portela03@gmail.com>
2022-05-27 17:05:01 -0400

    registration

--- frontend-wall-app/src/App.tsx
+++ frontend-wall-app/src/App.tsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="App">
-    <div style={{display: "flex", alignItems: "center", justifyContent:"space-around"}}>
+    {/* <div style={{display: "flex", alignItems: "center", justifyContent:"space-around"}}>
       <div>
         <h1>The Wall</h1>
       </div>
@@ -84,8 +84,8 @@ function App() {
           <p>{message.text}</p>
           </div>
         )}
-    </div>
-    
+    </div> */}
+    <Registration/>
     
   </div>
   );
--- frontend-wall-app/src/Registration.tsx
+++ frontend-wall-app/src/Registration.tsx
@@ -1,9 +1,75 @@
 import React, { useState } from 'react';
 
+type RegistrationForm = {
+    firstName: string,
+    lastName: string,
+    userName: string,
+    email: string,
+}
+
+const initialRegistration = {
+    firstName: "",
+    lastName:"",
+    userName:"",
+    email:"",
+}
+
+type FormValues = { 
+    firstName: {value:string},
+    lastName: {value:string},
+    userName: {value:string},
+    email: {value:string},
+} & EventTarget
+
+
 function Registration() {
 
+    const [registration, setRegistration] = useState<RegistrationForm>(initialRegistration)
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => { 
+        e.preventDefault()
+        console.log("i have been submitted")
+        const customTarget = e.target
+    }
+
     return (
-        <h1>Register</h1>
+       <div>
+       <div style={{display: "flex", alignItems: "center", justifyContent:"space-around"}}>
+        <div>
+          <h1>The Wall</h1>
+        </div>
+        <div>
+          <button>Log in</button>
+        </div>
+      </div>
+
+      <div>
+           <h1>Sign up to write on the wall!</h1>
+       </div>
+
+       <form 
+        style=
+            {
+                {
+                    display: "flex", 
+                    justifyContent: "center", 
+                    alignItems: "center", 
+                    flexDirection: "column"
+                }
+            }
+        onSubmit={(e) => handleFormSubmit(e)}
+        >
+           <input type= "text" name='firstName' placeholder='First name'></input>
+           <input type= "text" name='lastName' placeholder='Last name'></input>
+           <input type= "text" name='userName' placeholder='User name'></input>
+           <input type= "text" name='email' placeholder='Email'></input>
+           <br/>
+           <button>Sign me up!</button>
+       </form >
+       <h3>or</h3>
+       <button>View wall as a guest</button>
+      </div>
+     
     );
 }
 
