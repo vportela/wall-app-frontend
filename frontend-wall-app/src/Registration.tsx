a6646dfeb4b64fe5baa7f65def2595a650132d99
vportela <victoria.portela03@gmail.com>
2022-06-02 12:04:30 -0400

    on success registration redirect to home page

--- frontend-wall-app/src/Registration.tsx
+++ frontend-wall-app/src/Registration.tsx
@@ -32,70 +32,65 @@ type FormValues = {
 
 function Registration() {
 
-    const [registration, setRegistration] = useState<RegistrationForm[]>([])
+  let navigate = useNavigate();
 
 
-    useEffect(() => { 
-        console.log("in useEffect")
-        axios.get<RegistrationForm[]>("http://localhost:5000/registration")
+  const [registration, setRegistration] = useState<RegistrationForm[]>([])
+  const [registrationErrorMessage, setRegistrationErrorMessage] = useState<string>("")
+
+
+  useEffect(() => { 
+      console.log("in useEffect")
+      axios.get<RegistrationForm[]>("http://localhost:5000/registration")
+        .then((response) => { 
+          console.log("hooray it was successful!! with response", response)
+          const result = response.data
+          console.log("result from response ", result)
+          setRegistration(response.data)
+        })
+        .catch(() => {
+          console.log("uh oh! something went wrong.")
+        })
+    }, [])
+
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => { 
+      e.preventDefault()
+      console.log("i have been submitted")
+      const customTarget = e.target as FormValues
+
+      let firstName = customTarget.firstName.value
+      let lastName = customTarget.lastName.value
+      let userName = customTarget.userName.value
+      let email = customTarget.email.value
+
+      const lastInArray = registration[registration.length - 1]
+
+      const newUser = { 
+          id: lastInArray.id + 1,
+          firstName: firstName,
+          lastName: lastName,
+          userName: userName,
+          email: email
+      }
+
+      console.log("newUser", newUser)
+
+      axios.post<RegistrationForm>("http://localhost:5000/registration", newUser) //api call -
           .then((response) => { 
-            console.log("hooray it was successful!! with response", response)
-            const result = response.data
-            console.log("result from response ", result)
-            setRegistration(response.data)
+          console.log("New user successfully created! with response", response)
+          setRegistration([ ...registration, response.data,])
+          {navigate("/")}
+          
+          
+          })//if call is successful, this line runs
+          .catch((err) => {
+          console.log("there was an error")
+          setRegistrationErrorMessage("Could not sign you up, please check all fields and try again!")
+
           })
-          .catch(() => {
-            console.log("uh oh! something went wrong.")
-          })
-      }, [])
+  }
 
-    const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => { 
-        e.preventDefault()
-        console.log("i have been submitted")
-        const customTarget = e.target as FormValues
-
-        let firstName = customTarget.firstName.value
-        let lastName = customTarget.lastName.value
-        let userName = customTarget.userName.value
-        let email = customTarget.email.value
-
-        const lastInArray = registration[registration.length - 1]
-
-        const newUser = { 
-            id: lastInArray.id + 1,
-            firstName: firstName,
-            lastName: lastName,
-            userName: userName,
-            email: email
-        }
-
-        console.log("newUser", newUser)
-
-        axios.post<RegistrationForm>("http://localhost:5000/registration", newUser) //api call -
-            .then((response) => { 
-            console.log("New user successfully created! with response", response)
-            setRegistration([ ...registration, response.data,])
-            
-            })//if call is successful, this line runs
-            .catch((err) => {
-            console.log("there was an error")
-            })
-    }
-
-    // const handleClick = () => { 
-    //     console.log("I will take u back to the main page")
-    // }
-
-    let navigate = useNavigate();
-
-    // const HandleClick = () => { 
-    //   const handleClick = async (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    //     e.preventDefault()
-    //     await (e.target);
-    //     navigate("/", {replace: true});
-    //   }
-    // }
-
+    
 
     return (
        <div>
@@ -130,6 +125,7 @@ function Registration() {
            <input type= "text" name='email' placeholder='Email'></input>
            <br/>
            <button>Sign me up!</button>
+           <h5 style={{color: "red"}}>{registrationErrorMessage}</h5>
        </form >
        <h3>or</h3>
        <button onClick={() => {navigate("/")}}>View wall as a guest</button>
